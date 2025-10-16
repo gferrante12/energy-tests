9734eaa9921fd8ed8fefac56f708bdc9dbb1548b
gferrante12 <g.ferrante12@campus.unimib.it>
2025-10-16 15:19:26 +0200

    Add energy_conversion function to convert Joules to kilocalories

---
+++ mymodule.py
@@ -0,0 +1,5 @@
+def energy_conversion(joules: float) -> float:
+    """Convert energy from Joules to kilocalories."""
+    if joules < 0:
+        raise ValueError("Energy cannot be negative")
+    return joules / 4184.0
